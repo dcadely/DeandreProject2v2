278a39b8def6e7e585163da4e17561f523ca6c29
deandre <you@example.com>
2022-10-04 15:25:02 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+Student_ID = input("student ID:")
+
+num_credits=input("gpa?:")
+print(f"All student ID numbers are: \n00102342,\n00323123,\n00301110,\n00205323,\n00432529,\n00499823,\n00010002")
+print(f"All student GPA: \n3.2,\n3.9,\n3.3,\n3.7,\n3.5,\n3.8,\n3.2")
